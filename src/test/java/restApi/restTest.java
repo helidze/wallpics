73895447a8f53a3160885cc164b3f0618506baa2
George Helidze <>
2018-05-30 13:26:06 +0300

    newTest

---
+++ src/test/java/restApi/restTest.java
@@ -0,0 +1,30 @@
+package restApi;
+
+import io.restassured.RestAssured.*;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.hasItems;
+
+public class restTest {
+
+    @Test
+    public void testStatusCode(){
+        given().
+            get("https://www.wallpics.com").
+        then().
+            assertThat().
+            statusCode(200).
+            log().status();
+    }
+
+    @Test
+    public void testOrderWallpicsExist(){
+        given().
+            get("https://www.wallpics.com").
+        then().
+            assertThat().
+            body(containsString("Order Wallpics"));
+    }
+}
